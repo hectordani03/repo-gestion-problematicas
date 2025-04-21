0de762d2a4925ac9b5715a4a8e563ae1dbdb9fd3
hectordani03 <hectordaniel010203@gmail.com>
2025-04-21 11:25:52 -0600

    Creacion de estructura base dashboard

--- apps/web/src/features/dashboard/pages/DashboardMain.jsx
+++ apps/web/src/features/dashboard/pages/DashboardMain.jsx
@@ -1,8 +1,27 @@
-export default function DashboardMain() {
-    return (
-        <div>
-        <h1>Dashboard</h1>
-        <p>Welcome to the dashboard!</p>
+import ProjectStats from '../components/ProjectStats'
+import ProjectList from '../components/ProjectList'
+import RecentProjects from '../components/RecentProjects'
+import RecentActivity from '../components/RecentActivity'
+import ProjectSummary from '../components/ProjectSummary'
+
+const DashboardMain = () => {
+  return (
+    <div className="p-4 grid gap-4 xl:grid-cols-3 w-8/12">
+      <div className="col-span-3 items-start">
+        <ProjectStats />
+      </div>
+      <div className="col-span-3 lg:col-span-1">
+        <ProjectList />
+      </div>
+      <div className="col-span-3 lg:col-span-2 grid gap-4">
+        <RecentProjects />
+        <div className="grid md:grid-cols-2 gap-4">
+          <RecentActivity />
+          <ProjectSummary />
         </div>
-    )
-    }
+      </div>
+    </div>
+  )
+}
+
+export default DashboardMain
