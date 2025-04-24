ef7f96a64532f786945c9952b7af7d54297e0e61
Zama15 <alexavdstiles@gmail.com>
2025-04-24 13:14:07 -0600

    Fix typos
    Added the generated prisma client to gitignore since this is generated
    automatically when do it the migration pull to the db

--- packages/domain/registerUser.js
+++ packages/domain/registerUser.js
@@ -5,12 +5,13 @@ import { userRepo } from "@reuc/infrastructure/userRepo.js";
 export async function registerUser(data) {
   const existing = await userRepo.findByEmail(data.email);
   if (existing) throw new Error("El correo ya ha sido usado.");
-
+  console.time("Hashing");
   const hashed = await hashPassword(data.password);
+  console.timeEnd("Hashing");
 
   const user = new User({ ...data, password: hashed });
 
-  const saved = userRepo.save(user.toPrimitives);
+  const saved = await userRepo.save(user.toPrimitives());
   if (!saved.uuid_user) throw new Error("Hubo un error al crear el usuario");
 
   return saved;
