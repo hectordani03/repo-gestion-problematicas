40ece8b39bc98adb32cf9f67e71e293200253a70
Zama15 <alexavdstiles@gmail.com>
2025-04-24 01:49:54 -0600

    Add dependencies
    This commit is just a trash file staged, but I was afraid to lose
    changes if I make another action that it is not a commit, so here we
    are.

--- apps/web/vite.config.js
+++ apps/web/vite.config.js
@@ -1,14 +1,20 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
-import react from '@vitejs/plugin-react-swc'
-import path from 'path'
+import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+  server: {
+    proxy: {
+      "/auth": "http://localhost:3000",
+      "/csrf-token": "http://localhost:3000",
+    },
+  },
+});
