acf6e08117ecdc6b3ae01e6e94a1bd2343b6ef54
hectordani03 <hectordaniel010203@gmail.com>
2025-04-25 12:23:19 -0600

    Refactor register correct

--- packages/domain/entities/User.js
+++ packages/domain/entities/User.js
@@ -3,6 +3,7 @@ export class User {
     uuid_user = undefined,
     email,
     password,
+    studentId = undefined,
     firstName = undefined,
     middleName = undefined,
     lastName = undefined,
@@ -12,6 +13,7 @@ export class User {
   }) {
     this.email = email.toLowerCase();
     this.password = password;
+    this.studentId = studentId;
 
     if (status != undefined && typeof status != "number")
       throw new Error("Status must be a number");
