c40a19a84743d2bf91f28950ad904e419ad08d8e
hectordani03 <hectordaniel010203@gmail.com>
2025-04-25 12:23:19 -0600

    Refactor register correct

--- packages/application/auth/register.js
+++ packages/application/auth/register.js
@@ -9,12 +9,15 @@ export async function register(data) {
     const passwordError = validatePassword(data.password);
     if (passwordError) throw new ValidationError(passwordError);
 
-    if (!passwordError) {
-      const passConfirmError = validatePasswordConfirm(
-        data.password,
-        data.confirmPassword
-      );
-      if (passConfirmError) throw new ValidationError(passConfirmError);
+    const passConfirmError = validatePasswordConfirm(
+      data.password,
+      data.confirmPassword
+    );
+    if (passConfirmError) throw new ValidationError(passConfirmError);
+    
+    if (data.email.endsWith("@ucol.mx")) {
+      const studentIdError = validateStudentId(data.studentId);
+      if (studentIdError) throw new ValidationError(studentIdError);
     }
 
     const user = await registerUser(data);
@@ -30,23 +33,30 @@ export async function register(data) {
 function validateEmail(email) {
   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   if (!email) return "Email necesario para registro.";
-  if (!emailRegex.test(email)) return "Email no valido.";
+  if (!emailRegex.test(email)) return "Email no válido.";
   return null;
 }
 
 function validatePassword(password) {
-  if (!password) return "Contraseña necesaria para el registro.";
-  if (password.length < 8) return "Contraseña de al menos 8 caracteres.";
+  if (!password) return "Contraseña obligatoria.";
+  if (password.length < 8) return "Mínimo 8 caracteres.";
   if (!/[A-Z]/.test(password))
-    return "Contraseña debe tener al menos una letra mayuscula.";
+    return "Debe incluir al menos una mayúscula.";
   if (!/[a-z]/.test(password))
-    return "Contraseña debe tener al menos una letra minuscula.";
+    return "Debe incluir al menos una minúscula.";
   if (!/[0-9!@#$%^&*]/.test(password))
-    return "Contraseña debe tener un numero o un simbolo.";
+    return "Debe incluir un número o símbolo.";
   return null;
 }
 
-function validatePasswordConfirm(password, passwordConfirm) {
-  if (password !== passwordConfirm) return "Contraseñas no coinciden.";
+function validatePasswordConfirm(password, confirmPassword) {
+  if (password !== confirmPassword) return "Las contraseñas no coinciden.";
+  return null;
+}
+
+function validateStudentId(studentId) {
+
+  if (studentId.length !== 8 && studentId.length !== 6) return "El número de cuenta debe tener 8 dígitos (alumno) o 6 (maestro)."
+  if (!/^\d+$/.test(studentId)) return "El número de cuenta solo debe contener números."
   return null;
 }
