5b6719868fce5cbc81fe9eef609959866d9ef46d
Zama15 <alexavdstiles@gmail.com>
2025-05-29 17:40:01 -0600

    hotfix(register&login): Client error

--- packages/domain/user/registerUser.js
+++ packages/domain/user/registerUser.js
@@ -5,12 +5,29 @@ import { userRepo } from "@reuc/infrastructure/userRepo.js";
 import { createOutsider } from "../outsider/createOursider.js";
 import { createStudent } from "../student/createStudent.js";
 import { createProfessor } from "../professor/createProfessor.js";
+import { ConflictError } from "../errors/ConflictError.js";
 
 export async function registerUser({ body, ip, userAgent }) {
   const existing = await userRepo.findByEmail(body.email);
-  if (existing) throw new Error("El correo ya ha sido usado.");
+  if (existing) throw new ConflictError("El correo ya ha sido usado.");
 
   const hashed = await hashPassword(body.password);
+
+  let roleCreator;
+  let rolePrefix;
+
+  if (!body.universityId) {
+    roleCreator = createOutsider;
+    rolePrefix = "outsider";
+  } else if (body.universityId.length === 8) {
+    roleCreator = (uuid) => createStudent(uuid, body.universityId);
+    rolePrefix = "student";
+  } else if (body.universityId.length === 4) {
+    roleCreator = (uuid) => createProfessor(uuid, body.universityId);
+    rolePrefix = "professor";
+  } else {
+    throw new ConflictError("Falta numeró de cuenta.");
+  }
 
   const user = new User({
     ...body,
@@ -21,28 +38,8 @@ export async function registerUser({ body, ip, userAgent }) {
 
   const saved = await userRepo.save(user.toPrimitives());
 
-  let uuidRole;
-
-  if (!body.universityId) {
-    const outsiderSaved = await createOutsider(saved.uuid_user);
-    uuidRole = `outsider:${outsiderSaved.uuid_outsider}`;
-  } else if (body.universityId.length === 8) {
-    const studentSaved = await createStudent(
-      saved.uuid_user,
-      body.universityId
-    );
-    uuidRole = `student:${studentSaved.uuid_student}`;
-  } else if (body.universityId.length === 4) {
-    const professorSaved = await createProfessor(
-      saved.uuid_user,
-      body.universityId
-    );
-    uuidRole = `professor:${professorSaved.uuid_professor}`;
-  } else {
-    throw new Error(
-      "Unable to assign a role. Please ensure the provided information is correct and complete."
-    );
-  }
+  const role = await roleCreator(saved.uuid_user);
+  const uuidRole = `${rolePrefix}:${role[`uuid_${rolePrefix}`]}`;
 
   const payload = {
     uuid_user: saved.uuid_user,
